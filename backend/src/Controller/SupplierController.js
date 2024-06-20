d1d9840ff48a947d3ac3bd4fd23d289e4e78c0e4
vThenujan <vthenujan7400@gmail.com>
2024-06-20 14:53:24 +0530

    supplier signup

--- backend/src/Controller/SupplierController.js
+++ backend/src/Controller/SupplierController.js
@@ -1,5 +1,29 @@
 var supplierModels = require('../Model/SupplierModel')
 
-exports.suppliersSignin = () => {
-    
+exports.suppliersSignin = async (req, res) => {
+   try{
+        const existing_user = supplierModels.findOne({email : req.body.email})
+        if(existing_user){
+            return res.status(400).json({
+                error: 'Email already exists' 
+            })
+        }
+
+        const createSupplier = new supplierModels({
+            supplier_name : req.body.supplier_name,
+            email : req.body.email,
+            location : req.body.location,
+            work_for : req.body.work_for,
+            password : req.body.password,
+       })
+
+        const createRes = await createSupplier.save()
+        return res.status(200).send(createRes)
+   }
+   catch(error){
+        return res.status(500).json({
+            error : 'can not create account',
+            detail : error.message
+        })
+   }
 }
--- backend/src/Model/SupplierModel.js
+++ backend/src/Model/SupplierModel.js
@@ -1,7 +1,57 @@
-const dbConnection = require('../Confic/Db.confic')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
 
 
+const supplierSchema = new mongoose.Schema({
+    supplier_name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    location : {
+        type : String,
+        required : true
+    },
+    work_for : {
+        type : String
+    },
+    password : {
+        type : String
+    }, 
+    createdAt : {
+        type : Date,
+        default : Date.now
+    },
+    updatedAt  : {
+        type : Date,
+        default : Date.now
+    },
+    role : {
+        type : String,
+        default : 'supplier'
+    }
+
+})
+
+supplierSchema.pre('save', async function(next) {
+    const supplier = this;
+    if (!supplier.isModified('password')) {
+        return next()
+    }
+    try {
+        const hash = await bcrypt.hash(supplier.password, 10)
+        supplier.password = hash
+        next()
+    } catch (error) {
+        return next(error);
+    }
+});
 
 
+const supplier = mongoose.model("supplier", supplierSchema)
 
-// module.exports = schedules
+module.exports = supplier
